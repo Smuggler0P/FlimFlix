84554e01834fbcfed8f54c8e0f47707d8f132d2f
Smuggler0P <avimathur789@gmail.com>
2024-06-19 01:42:29 +0530

    Created working Header using index.html and global.js

---
+++ js/global.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (const elem of elements) elem.addEventListener(eventType, callback);
+};
+
+const searchBox = document.querySelector("[search-box]");
+const searchTogglers = document.querySelectorAll("[search-toggler]");
+
+addEventOnElements(searchTogglers, "click", function () {
+  searchBox.classList.toggle("active");
+});
